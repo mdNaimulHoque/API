d91e2e3bfea43edd81ff0aaadb8132f45d17d43a
mdNaimulHoque <naimulhoq89@gmail.com>
2020-09-02 21:12:36 +0600

    time out is done

---
+++ timeout.js
@@ -0,0 +1,14 @@
+function doSomething(){
+    console.log(3333);
+}
+console.log(2222);
+// setTimeout(() => {
+//     console.log('See you later')
+// }, 5);
+setInterval(() => {
+    console.log('do it again')
+}, 3000);
+console.log(4444);
+console.log(4444);
+console.log(4444);
+console.log(4444);
